11c7107dfcd4d9c30eab35a060ec3a22f32788ff
Riobener <v.shevchenko@energochain.ru>
2023-05-08 19:48:55 +0300

    Side menu designed

--- app/src/main/java/com/riobener/sonicsoul/MainActivity.kt
+++ app/src/main/java/com/riobener/sonicsoul/MainActivity.kt
@@ -2,6 +2,7 @@ package com.riobener.sonicsoul
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import com.riobener.sonicsoul.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,8 +11,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.drawerButton.setOnClickListener {
+            binding.drawerLayout.openDrawer(GravityCompat.START)
+        }
         setContentView(binding.root)
     }
-
 }
